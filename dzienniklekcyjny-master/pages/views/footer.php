92f852ed35e3a6479174b40857f5ed7eccdd9c6c
PKubis <126944513+PKubis@users.noreply.github.com>
2023-07-08 20:28:40 +0200

    Delete dzienniklekcyjny-master directory

--- dzienniklekcyjny-master/pages/views/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="main-footer">
-	<strong>Iza, Marcin, Piotr <a href="https://github.com/kurasmarcin/dzienniklekcyjny/tree/master" target="_blank">Nasze Repozytorium</a>. </strong> Praca dzięki Admin_LTE. <div class="float-right d-none d-sm-inline-block">
-		<b>Wersja</b> 5.8.9
-	</div>
-</footer>
